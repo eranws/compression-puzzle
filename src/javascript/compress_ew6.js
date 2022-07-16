14f25b38bf7e669aec175af832067c55dd926c07
eran ws <eranws@gmail.com>
2022-07-16 16:47:52 +0300

    ew6 map+replace - clean: 105 chars

--- src/javascript/compress_ew6.js
+++ src/javascript/compress_ew6.js
@@ -1,35 +1,11 @@
 const assert = require("assert");
 
-// compress = (s, k = [...s]) => {
-//   s = 1 + k.join(1);
-//   m = (c) => k.map(c);
-//   m((a) => m((_, i) => m((_, j) => (s = s.replaceAll(i + a + j + a, i + j + a)))));
-//   return s;
-// };
-
-// compress = (s, k = [...s], m = (c) => k.map(c)) => {
-//   s = 1 + k.join(1);
-//   return m((a) => m((_, i) => m((_, j) => (s = s.replaceAll(i + a + j + a, i + j + a))))).pop()[0][0];
-// };
-
-// compress = (s, k = [...s], m = (c) => k.map(c), t = 1 + k.join(1)) =>
-  // m((a) => m((_, i) => m((_, j) => (t = t.replaceAll(i + a + j + a, i + j + a))))).pop()[0][0];
-
+//     v-- 105 char ---
 compress=(s,k=[...s],m=c=>k.map(c),t=1+k.join(1))=>[m(a=>m((_,i)=>m((_,j)=>(t=t.replace(i+a+j+a,i+j+a))))),t][1]
-                     
-// compress = (s, k = [...s], m = (c) => k.map(c), t = 1 + k.join(1)) =>
-//   m((a) =>
-//   m((_, i) =>
-//   m((_, j) =>
-//   t = t.replace(i + a + j + a, i + j + a)
-//   )))
-//     .pop()
-//     .pop()
-//     .pop();
 
 test = assert.equal;
-// test = console.log;
-// test(compress("AAA"), "3A");
+
+test(compress("AAA"), "3A");
 test(compress("AAAAAAAAAAAAAAAAA"), "17A");
 test(compress("AAABBAAC"), "3A2B2A1C");
 test(compress("AAAAABBCCC"), "5A2B3C");
